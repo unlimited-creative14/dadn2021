87fb0e09d09c32836bee671f51e30ea90da5efc4
long <nguyenphilong.bkevent@gmail.com>
2021-05-12 20:08:12 +0700

    add MQTT server & fix gradle bug

--- app/src/main/java/com/hk203/dadn/MainActivity.java
+++ app/src/main/java/com/hk203/dadn/MainActivity.java
@@ -20,6 +20,9 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.hk203.dadn.databinding.ActivityMainBinding;
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
 
 import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
 import org.eclipse.paho.client.mqttv3.MqttCallbackExtended;
@@ -92,17 +95,18 @@ public class MainActivity extends AppCompatActivity {
         } catch (MqttException e) {
 
         }
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
+                new DataPoint(0, 1),
+                new DataPoint(1, 5),
+                new DataPoint(2, 3),
+                new DataPoint(3, 2),
+                new DataPoint(4, 6)
+        });
+        graph.addSeries(series);
     }
 
-//		GraphView graph = (GraphView) findViewById(R.id.graph);
-//		LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
-//				new DataPoint(0, 1),
-//				new DataPoint(1, 5),
-//				new DataPoint(2, 3),
-//				new DataPoint(3, 2),
-//				new DataPoint(4, 6)
-//		});
-//		graph.addSeries(series);
+
 
     void preCreate() {
         setSupportActionBar(binding.appBarMain.toolbar);
