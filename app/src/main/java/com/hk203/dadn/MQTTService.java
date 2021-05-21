f4442f0bbf685462369d791721ea7a8ac253eb2f
NguyenLongKim <longkimpyxq2@gmail.com>
2021-05-21 10:54:14 +0700

    temperature realtime chart ver1.0

--- app/src/main/java/com/hk203/dadn/MQTTService.java
+++ app/src/main/java/com/hk203/dadn/MQTTService.java
@@ -14,59 +14,57 @@ import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 
 public class MQTTService {
-    final String serverUri = "tcp://io.adafruit.com:1883";
-    final String clientId = "myID";
-    final String subscriptionTopic = "malongnhan/feeds/server";
-    public static final String default_username = "malongnhan";
-    public static final String default_io_key = "aio_oRCT69g6V2ainDyuWPQP6QORyiwG";
+    private String serverUri = "tcp://io.adafruit.com:1883";
+    private String clientId = "myID";
+    private String subscriptionTopic = "kimnguyenlong/feeds/temp";
+    private String username = "kimnguyenlong";
+    private String io_key = "aio_mpYq21jymEgrHUlt5MLMNtZaCLnQ";
+    private MqttAndroidClient mqttAndroidClient;
 
-    public static final MqttCallbackExtended default_callback = new MqttCallbackExtended() {
-        @Override
-        public void connectComplete(boolean b, String s) {
-            Log.w("mqtt", s);
-        }
-
-        @Override
-        public void connectionLost(Throwable throwable) {
-        }
-
-        @Override
-        public void messageArrived(String topic, MqttMessage message) throws Exception {
-            Log.w("Mqtt", message.toString());
-        }
-
-        @Override
-        public void deliveryComplete(IMqttDeliveryToken iMqttDeliveryToken) {
-
-        }
-    };
-
-    public MqttAndroidClient mqttAndroidClient;
-
-    public MQTTService(Context context, String username, String io_key, MqttCallbackExtended callback)
+    public MQTTService(Context context)
     {
         mqttAndroidClient = new MqttAndroidClient(context, serverUri, clientId);
-        mqttAndroidClient.setCallback(callback);
+        /*mqttAndroidClient.setCallback(new MqttCallbackExtended() {
+            @Override
+            public void connectComplete(boolean reconnect, String serverURI) {
+                Log.w("Mqtt","connect complete");
+            }
+
+            @Override
+            public void connectionLost(Throwable cause) {
+                Log.w("Mqtt","connect lost");
+                try{
+                    mqttAndroidClient.unsubscribe(subscriptionTopic);
+                } catch (MqttException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void messageArrived(String topic, MqttMessage message) throws Exception {
+                Log.w("Mqtt",message.toString());
+            }
+
+            @Override
+            public void deliveryComplete(IMqttDeliveryToken token) {
+
+            }
+        });*/
         connect(username, io_key);
     }
 
-    public MQTTService(Context context) {
-        this(context, default_username, default_io_key, default_callback);
-    }
-
-    public void setCallback(MqttCallbackExtended callback) {
-        mqttAndroidClient.setCallback(callback);
+    public void setCallBack(MqttCallbackExtended newCallBack){
+        mqttAndroidClient.setCallback(newCallBack);
     }
 
     private void connect(String username, String io_key) {
         MqttConnectOptions mqttConnectOptions = new MqttConnectOptions();
-        mqttConnectOptions.setAutomaticReconnect(true);
-        mqttConnectOptions.setCleanSession(false);
+        mqttConnectOptions.setAutomaticReconnect(false);
+        //mqttConnectOptions.setCleanSession(false);
         mqttConnectOptions.setUserName(username);
         mqttConnectOptions.setPassword(io_key.toCharArray());
 
         try {
-
             mqttAndroidClient.connect(mqttConnectOptions, null, new IMqttActionListener()
             {
                 @Override
--- app/src/main/java/com/hk203/dadn/ui/patient_info/PatientInfoFragment.java
+++ app/src/main/java/com/hk203/dadn/ui/patient_info/PatientInfoFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,9 +13,15 @@ import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
+import com.hk203.dadn.MQTTService;
 import com.hk203.dadn.R;
 import com.hk203.dadn.databinding.FragmentPatientInfoBinding;
 import com.hk203.dadn.databinding.FragmentPatientListBinding;
+
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.MqttCallbackExtended;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -40,39 +47,56 @@ public class PatientInfoFragment extends Fragment {
         LineData lineData = new LineData(dataSet);
         lc_temp.setData(lineData);
 
+        MQTTService mqttService = new MQTTService(getContext());
+        mqttService.setCallBack(new MqttCallbackExtended() {
+            @Override
+            public void connectComplete(boolean reconnect, String serverURI) {
+                Log.w("Mqtt","connect complete");
+            }
 
+            @Override
+            public void connectionLost(Throwable cause) {
+                Log.w("Mqtt","connect lost");
+            }
+
+            @Override
+            public void messageArrived(String topic, MqttMessage message) throws Exception {
+                Log.w("Mqtt",message.toString());
+                updateChart(Float.valueOf(message.toString()));
+            }
+
+            @Override
+            public void deliveryComplete(IMqttDeliveryToken token) {
+
+            }
+        });
         return binding.getRoot();
     }
 
     @Override
     public void onResume() {
         super.onResume();
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                for (int i=0;i<50;i++){
-                    getActivity().runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            addEntry();
-                        }
-                    });
-                    try {
-                        Thread.sleep(2000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        }).start();
     }
 
-    private void addEntry(){
+    private void addEntry(float newTemp){
         LineData data = lc_temp.getLineData();
-        Random rd = new Random();
-        data.addEntry(new Entry(data.getEntryCount(), rd.nextInt(10)),0);
+        data.addEntry(new Entry(data.getEntryCount(), newTemp),0);
         lc_temp.notifyDataSetChanged();
         lc_temp.setVisibleXRange(0,6);
         lc_temp.moveViewToX(data.getEntryCount()-6);
+    }
+
+    private void updateChart(float newTemp){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                    getActivity().runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            addEntry(newTemp);
+                        }
+                    });
+            }
+        }).start();
     }
 }
